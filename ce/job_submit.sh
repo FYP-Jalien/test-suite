e934f8b5714361b95e27163622a28ad57cfd5470
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-01-23 14:16:22 +0530

    Add content validation.

--- ce/job_submit.sh
+++ ce/job_submit.sh
@@ -1,6 +1,34 @@
+#!/bin/bash
+
+source ../func/messages.sh
+
 current_date=$(date +'%Y-%m-%d')
 directory_path="/home/submituser/htcondor/$current_date"
 container_name="shared_volume-JCentral-dev-CE-1"
 
+# Get the most recent .jdl file
+most_recent_jdl=$(sudo docker exec -it "$container_name" bash -c "ls -t $directory_path/*.jdl | head -n 1")
 
-files=$(sudo docker exec -it "$container_name" bin/bash -c "ls $directory_path")
+# Check if the most recent .jdl file exists
+if [ -n "$most_recent_jdl" ]; then
+    print_success "Success. jdl file found: $most_recent_jdl"
+else
+    print_error "Error. No jdl file found."
+    exit 1
+fi
+
+# Function to validate the content of the most recent .jdl file.
+validate_content(){
+    if sudo docker exec -it "$container_name" /bin/bash -c "grep -q "$1" '$most_recent_jdl'"; then
+        print_success "Success. $1 found in .jdl file."
+    else
+        print_error "$1 not found in .jdl file."
+        exit 1
+    fi
+}
+
+# Validate the content of the most recent .jdl file.
+validate_content "cmd"
+
+
+
