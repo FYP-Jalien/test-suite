835c7d86e05f3077c15f0cd6b61601236807ce75
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-01-26 18:05:00 +0530

    Update the checking criteria

--- ce/condor_validation.sh
+++ ce/condor_validation.sh
@@ -3,13 +3,20 @@ source ../func/messages.sh
 
 container_name="shared_volume-JCentral-dev-CE-1"
 
-# Log in to the Docker container and run the "condor_q" command
+# Log in to the Docker container and run the "condor_q" command.
 output1=$(sudo docker exec -it "$container_name" /bin/bash -c "condor_q")
-output2=$(sudo docker exec -it "$container_name" /bin/bash -c "condor_q")
+output2=$(sudo docker exec -it "$container_name" /bin/bash -c "condor_status")
 
-# Check if both outputs contain the specified substring
-if [[ $output1 == *"-- Schedd: localhost.localdomain"* && $output2 == *"-- Schedd: localhost.localdomain"* ]]; then
-    print_success "Success! condor_q and condor_status is working."
+# Check if condor_q is working.
+if [[ "$output1" == *"-- Schedd: localhost.localdomain"* ]]; then
+    print_success "Success! condor_q is working."
 else
-    print_error "Error! condor_q or condor_status is not working."
+    print_error "Error! condor_q is not working."
+fi
+
+# Check if condor_status is working.
+if [[ "$output2" == *"Name"* ]]; then
+    print_success "Success! condor_status is working."
+else
+    print_error "Error! condor_status is not working."
 fi
