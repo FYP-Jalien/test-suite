d1132645431ac301952a963577afff3deec5e31e
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-01-30 14:03:13 +0530

    Add state change conditions.

--- job_submission/check_job_submission.sh
+++ job_submission/check_job_submission.sh
@@ -5,7 +5,7 @@ source ../func/messages.sh
 alienv_path="./alma-alienv"
 sample_path="./sample_test.jdl"
 testscript_path="./testscript_test.sh"
-shared_volume_env="/home/jananga/FYP/SHARED_VOLUME/env_setup.sh"
+shared_volume_env="/home/malith/Documents/FYP/SHARED_VOLUME/env_setup.sh"
 
 # Check if the alienv script exists
 if [ ! -f "$alienv_path" ]; then
@@ -34,7 +34,6 @@ if [ ! -f "$shared_volume_env" ]; then
 fi
 
 print_success "All files and directories exist."
-
 
 source $alienv_path setenv xjalienfs
 if [ $? -ne 0 ] ; then
@@ -101,36 +100,76 @@ function get_job_state(){
     echo "$job_row" | awk '{split($0, a); print a[NF-1]}'
 }
 
+# Function to test state of the job moves to D from W.
+function validate_job_finished(){
+    if [[ $state == "W" ]]; then
+        print_success "$job_id is in $state State."
+        echo "Waiting 5 minutes to complete the job."
+        sleep 300
+        state=$(get_job_state "$job_id")
+        if [ $? -ne 0 ] ; then
+            print_error "Job ID $job_id either not found or has a different format."
+            exit 1
+        fi
+
+        if [[ -n $state ]]; then
+            if [[ $state == "W" ]]; then
+                print_error "$job_id still is W state. Please check the logs"
+                exit 1
+            elif [[ $state == "D" ]]; then 
+                print_success "Job $job_id is in $state State."
+            elif [[ $state == "ESV" ]]; then
+                print_error "Job $job_id is in $state State. Error occurred when executing the job. This can be due to 
+                already existing output folder\n
+                Please check the logs for any other errors."
+            else
+                print_error "Invalid state for $job_id."
+                exit 1
+            fi
+        else
+            print_error "Invalid state for $job_id."
+            exit 1
+        fi
+    fi
+}
+
 state=$(get_job_state "$job_id")
 if [ $? -ne 0 ] ; then
-    print_error "Job ID $1 either not found or has a different format."
+    print_error "Job ID $job_id either not found or has a different format."
     exit 1
 fi
 if [[ -n $state ]]; then
     print_success "$job_id is in $state State."
 else
-    print_error "invalid state for $job_id."
+    print_error "Invalid state for $job_id."
     exit 1
 fi
 
 if [[ $state == "I" ]]; then
+    echo "Waiting 45 seconds to start the job."
     sleep 45
     state=$(get_job_state "$job_id")
     if [ $? -ne 0 ] ; then
-        print_error "Job ID $1 either not found or has a different format."
+        print_error "Job ID $job_id either not found or has a different format."
         exit 1
     fi
+
     if [[ -n $state ]]; then
         if [[ $state == "I" ]]; then
             print_error "$job_id still is I state. Probably Optimiser is not running."
             exit 1
         else
-            print_success "$job_id is in $state State."
+            validate_job_finished
         fi
     else
-        print_error "invalid state for $job_id."
+        print_error "Invalid state for $job_id."
         exit 1
     fi
+elif [[ $state == "W" ]]; then
+    validate_job_finished
+else
+    print_error "Invalid state for $job_id."
+    exit 1
 fi
 
 
