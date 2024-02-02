0129dc9b0862f2c42b50c89ced2f402971c1006f
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-02-02 18:16:15 +0530

    Add all test runner for Schedd.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if the script is run as root
+# Check if the script is run as root.
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root. Please use sudo."
     exit 1
@@ -8,6 +8,10 @@ fi
 
 # Running the common tests.
 cd common/
+source run_all_tests.sh
+
+# Running the schedd tests.
+cd schedd/
 source run_all_tests.sh
 
 
