2d53ed8315d4c155c19b84a7b7318980b78684aa
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-02-02 21:03:58 +0530

    change the echo calls to prints.

--- common/python3_validation.sh
+++ common/python3_validation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source ../func/messages.sh
+
 # Check if Python 3 is installed
 if command -v python3 &> /dev/null ; then
-    echo "Python 3 is installed on the system."
+    print_success "Python 3 is installed on the system."
 else
-    echo "Python 3 is not installed on the system. Please install Python 3 before proceeding."
+    print_error "Python 3 is not installed on the system. Please install Python 3 before proceeding."
 fi
