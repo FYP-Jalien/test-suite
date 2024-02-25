8b29d8ecfce7bea06f5c4c99232d9e7357bcf373
jananga99 <jananga1999@gmail.com>
2024-02-25 10:07:25 +0530

    condor_validations refactored

--- ce/index.sh
+++ ce/index.sh
@@ -5,3 +5,4 @@ set -e
 index_file_name="ce"
 
 source $index_file_name/container_validation.sh
+source $index_file_name/condor_validation.sh
--- worker/check-directory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ../.env
-
-sudo docker exec -it $CONTAINER_NAME_WORKER /bin/bash
-
-cd /var/lib/condor/execute
-
-ls -a
