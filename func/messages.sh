a3679c4968284dfa2423fc4cc8b18a5d75deec81
Jananga Kalawana <85403622+jananga99@users.noreply.github.com>
2024-03-20 00:29:13 +0530

    set test results to be shown in a table (#43)

--- func/messages.sh
+++ func/messages.sh
@@ -1,42 +1,80 @@
 #!/bin/bash
 
-success_color="\e[32m"
-error_color="\e[31m"
-reset_color="\e[0m"
+success_color="\e[32m" # green
+error_color="\e[31m"   # red
+reset_color="\e[0m"    # reset
 
-# Function to echo success message in green color
+# Function to print text with fixed width and multiline support
+function print_with_fixed_width() {
+    text="$1"
+    width=$2
+    printf "%-${width}s" "$text" | fold -w "$width" -s
+}
+
 function print_success() {
-    echo -e "$success_color$1$reset_color"
+    printf "| ${success_color}"
+    print_with_fixed_width "$1" 3
+    printf "${reset_color} | ${success_color}"
+    print_with_fixed_width "$2" 70
+    printf "${reset_color} | ${success_color}"
+    print_with_fixed_width "$3" 6
+    printf "${reset_color} | ${success_color}"
+    print_with_fixed_width "$4" 8
+    printf "${reset_color} |\n"
 }
 
-# Function to echo error message in red color
 function print_error() {
-    echo -e "$error_color$1$reset_color"
+    printf "| ${error_color}"
+    print_with_fixed_width "$1" 3
+    printf "${reset_color} | ${error_color}"
+    print_with_fixed_width "$2" 70
+    printf "${reset_color} | ${error_color}"
+    print_with_fixed_width "$3" 6
+    printf "${reset_color} | ${error_color}"
+    print_with_fixed_width "$4" 8
+    printf "${reset_color} | ${error_color}"
+    content_width=$(($(printf "%s" "$5" | wc -c) + 1))
+    min_width=100
+    if [[ $content_width -lt $min_width ]]; then
+        content_width=$min_width
+    fi
+    printf "${error_color}"
+    print_with_fixed_width "$5" $content_width
+    printf "${reset_color} |\n"
 }
 
-# Function to echo test header.
 function print_test_header() {
-    echo -e "\n====== Running all tests for $1 ======\n"
+    printf "Running all tests\n"
+    printf "| "
+    print_with_fixed_width "Id" 3
+    printf " | "
+    print_with_fixed_width "Name" 70
+    printf " | "
+    print_with_fixed_width "Status" 6
+    printf " | "
+    print_with_fixed_width "Level" 8
+    printf " | "
+    print_with_fixed_width "Message" 100
+    printf " |\n"
 }
-
 function print_full_test() {
     test_id=$1
     test_name=$2
     test_status=$3
     test_description=$4
-        test_level=$5
+    test_level=$5
     test_message=$6
-    if [[ "$test_status" == "PASSED" ]]; then
-        print_success "Test $test_id: $test_name - $test_status"
-    else
+
+    if [[ "$test_status" != "PASSED" ]]; then
         if [[ "$test_level" == "Critical" ]]; then
-            print_error "Test $test_id: $test_name - $test_status $test_level $test_description $test_message"
+            print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
             exit 1
         elif [[ "$test_level" == "Warning" ]]; then
-            print_error "Test $test_id: $test_name - $test_status $test_level $test_description $test_message"
+            print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
         elif [[ "$test_level" == "Minor" ]]; then
-            print_error "Test $test_id: $test_name - $test_status $test_level  $test_message"
+            print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
         fi
+    else
+        print_success "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
     fi
-
 }
