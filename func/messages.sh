b6634aa553fe79cf42388c6216bbd4297a4afe99
jananga99 <jananga1999@gmail.com>
2024-03-20 18:06:49 +0530

    basic test summary added

--- func/messages.sh
+++ func/messages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-success_color="\e[32m" # green
-error_color="\e[31m"   # red
-reset_color="\e[0m"    # reset
+success_color="\e[32m"     # green
+error_color="\e[31m"       # red
+warning_color='\033[1;33m' # yellow
+reset_color="\e[0m"        # reset
 
 # Function to print text with fixed width and multiline support
 function print_with_fixed_width() {
@@ -64,17 +65,35 @@ function print_full_test() {
     test_description=$4
     test_level=$5
     test_message=$6
-
+    if [[ "$test_level" == "Critical" ]]; then
+        critical_count=$((critical_count + 1))
+    elif [[ "$test_level" == "Warning" ]]; then
+        warning_count=$((warning_count + 1))
+    elif [[ "$test_level" == "Minor" ]]; then
+        minor_count=$((minor_count + 1))
+    fi
     if [[ "$test_status" != "PASSED" ]]; then
         if [[ "$test_level" == "Critical" ]]; then
+            critical_fail=$((critical_fail + 1))
             print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
             exit 1
         elif [[ "$test_level" == "Warning" ]]; then
+            warning_fail=$((warning_fail + 1))
             print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
         elif [[ "$test_level" == "Minor" ]]; then
+            minor_fail=$((minor_fail + 1))
             print_error "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
         fi
     else
         print_success "$test_id" "$test_name" "$test_status" "$test_level" "$test_message" "$test_description"
     fi
+}
+
+function print_test_summary() {
+    echo -e "Test Summary:"
+    echo -e "-----------------------------"
+    echo -e "Critical: ${error_color}$critical_count${reset_color} (${error_color}$critical_fail Failed${reset_color}, ${success_color}$critical_success Passed${reset_color})"
+    echo -e "Warning: ${warning_color}$warning_count${reset_color} (${warning_color}$warning_fail Failed${reset_color}, ${success_color}$warning_success Passed${reset_color})"
+    echo -e "Minor: ${reset_color}$minor_count${reset_color} (${reset_color}$minor_fail Failed${reset_color}, ${success_color}$minor_success Passed${reset_color})"
+    echo -e "-----------------------------"
 }
