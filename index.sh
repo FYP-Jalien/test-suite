7fd493b1a7b79df31b9080377c2442afb13395a7
Jananga Kalawana <85403622+jananga99@users.noreply.github.com>
2024-03-20 18:08:44 +0530

    basic test summary added (#45)

--- index.sh
+++ index.sh
@@ -5,6 +5,12 @@ source func/conversions.sh
 source .env
 
 id=0
+critical_count=0
+warning_count=0
+minor_count=0
+critical_fail=0
+warning_fail=0
+minor_fail=0
 
 print_test_header
 
@@ -14,5 +20,10 @@ source schedd/index.sh
 source se/index.sh
 source ce/index.sh
 source worker/index.sh
-source job_flow/index.sh
+# source job_flow/index.sh
 
+critical_success=$((critical_count - critical_fail))
+warning_success=$((warning_count - warning_fail))
+minor_success=$((minor_count - minor_fail))
+
+print_test_summary
