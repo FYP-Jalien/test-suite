ff8c595c1b10770d5c171c3177ae4413b87a715b
jananga99 <jananga1999@gmail.com>
2024-03-26 22:55:12 +0530

    Central Key store loading and LDAP settign tests added

--- index.sh
+++ index.sh
@@ -6,12 +6,12 @@ source .env
 
 id=0
 
-source common/index.sh
-source central/index.sh
-source schedd/index.sh
-source se/index.sh
-source ce/index.sh
-source worker/index.sh
-source job_flow/index.sh
+# source common/index.sh
+# source central/index.sh
+# source schedd/index.sh
+# source se/index.sh
+# source ce/index.sh
+# source worker/index.sh
+# source job_flow/index.sh
 source advanced_logs/index.sh
 
