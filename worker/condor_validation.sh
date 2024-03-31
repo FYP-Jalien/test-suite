64ba86046429bf87cd8e479aa9165e2700d72fdb
jananga99 <jananga1999@gmail.com>
2024-03-31 14:25:09 +0530

    Interactive terminlas removed

--- worker/condor_validation.sh
+++ worker/condor_validation.sh
@@ -5,7 +5,7 @@ name="Worker Container condor_status check"
 level="Critical"
 description="condor_status should give the status of the condor"
 
-condor_status_output=$(sudo docker exec -it "$CONTAINER_NAME_WORKER" /bin/bash -c condor_status)
+condor_status_output=$(sudo docker exec -t "$CONTAINER_NAME_WORKER" /bin/bash -c condor_status)
 
 if echo "$condor_status_output" | grep -q "slot" && echo "$condor_status_output" | grep -q "Total Owner Claimed Unclaimed Matched Preempting Backfill  Drain"; then
     print_full_test "$id" "$name" "PASSED" "$description" "$level" "condor_status is working"
