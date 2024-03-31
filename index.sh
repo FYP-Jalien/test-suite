a528ef37485a71d686d3243a9e656b0b859ad300
jananga99 <jananga1999@gmail.com>
2024-04-01 00:11:36 +0530

    Added the flags

--- index.sh
+++ index.sh
@@ -4,6 +4,26 @@ source func/messages.sh
 source func/conversions.sh
 source .env
 
+args=("$@")
+
+executeCommon=true
+executeContainer=true
+executeJobFlow=true
+executeAdvancedLogs=true
+
+for arg in "${args[@]}"; do
+    if [ "$arg" = "--host-only" ]; then
+        executeContainer=false
+        executeJobFlow=false
+        executeAdvancedLogs=false
+    elif [ "$arg" = "--container-only" ]; then
+        executeJobFlow=false
+        executeAdvancedLogs=false
+    elif [ "$arg" = "--flow-only" ]; then
+        executeAdvancedLogs=false
+    fi
+done
+
 id=0
 critical_count=0
 warning_count=0
@@ -14,14 +34,25 @@ minor_fail=0
 
 print_test_header
 
-source common/index.sh
-source central/index.sh
-source schedd/index.sh
-source se/index.sh
-source ce/index.sh
-source worker/index.sh
-source job_flow/index.sh
-source job_flow_logs/index.sh
-source advanced_logs/index.sh
+if [ $executeCommon = true ]; then
+    source common/index.sh
+fi
+
+if [ $executeContainer = true ]; then
+    source central/index.sh
+    source schedd/index.sh
+    source se/index.sh
+    source ce/index.sh
+    source worker/index.sh
+fi
+
+if [ $executeJobFlow = true ]; then
+    source job_flow/index.sh
+    source job_flow_logs/index.sh
+fi
+
+if [ $executeAdvancedLogs = true ]; then
+    source advanced_logs/index.sh
+fi
 
 print_test_summary
