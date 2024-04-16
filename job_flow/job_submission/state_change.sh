06a4ce1ab97fc1d83fb6e5a75e2791398afd3664
jananga99 <jananga1999@gmail.com>
2024-04-16 13:35:14 +0530

    Polling added for state change tests

--- job_flow/job_submission/state_change.sh
+++ job_flow/job_submission/state_change.sh
@@ -19,18 +19,31 @@ if [[ -z $state ]]; then
 fi
 
 if [[ $state == "I" ]]; then
-    echo "Waiting 45 seconds to start the job."
-    sleep 45
-    state=$(get_job_state "$job_id")
-    if [[ -z $state ]]; then
-        print_full_test "$id" "$name" "FAILED" "$description" "$level" "Job ID $job_id either not found or has a different format."
-    fi
-
-    if [[ $state == "I" ]]; then
-        print_full_test "$id" "$name" "FAILED" "$description" "$level" "Job $job_id is in $state State and should be moved to W. Check whether the optimiser is running and error logs. "
-    elif [[ $state == "W" ]] || [[ $state == "_D" ]] || [[ $state == "ASG" ]]; then
-        print_full_test "$id" "$name" "PASSED" "$description" "$level" "Job $job_id is in $state State."
-    fi
+    start_time=$SECONDS
+    timeout=$((SECONDS + 60))
+    while :; do
+        state=$(get_job_state "$job_id")
+        if [[ -z $state ]]; then
+            print_full_test "$id" "$name" "FAILED" "$description" "$level" "Job ID $job_id either not found or has a different format."
+            break
+        fi
+        if [[ $state == "W" ]] || [[ $state == "_D" ]] || [[ $state == "ASG" ]]; then
+            print_full_test "$id" "$name" "PASSED" "$description" "$level" "Job $job_id is in $state State."
+            break
+        elif [[ $state == "I" ]]; then
+            if [[ $SECONDS -gt $timeout ]]; then
+                print_full_test "$id" "$name" "FAILED" "$description" "$level" "Timeout reached. Job state did not transition to 'W' within 60 seconds."
+                break
+            else
+                echo "Current Job ID : $job_id"
+                "$JALIEN_PATH/jalien" -e ps
+                echo "Waiting for $(("$SECONDS" - "$start_time")) seconds"
+            fi
+        else
+            print_full_test "$id" "$name" "FAILED" "$description" "$level" "Unexpected state : $state"
+            break
+        fi
+    done
 elif [[ $state == "W" ]] || [[ $state == "_D" ]] || [[ $state == "ASG" ]]; then
     print_full_test "$id" "$name" "PASSED" "$description" "$level" "Job $job_id is in $state State."
 fi
@@ -39,19 +52,30 @@ id=$((id + 1))
 name="Job state transition check: from W to _D"
 level="Critical"
 description="Job should be moved from state W to state _D"
-echo "Waiting 5 minutes to complete the job."
-sleep 300
-state=$(get_job_state "$job_id")
-if [[ -z $state ]]; then
-    print_full_test "$id" "$name" "FAILED" "$description" "$level" "Job ID $job_id either not found or has a different format."
-fi
+timeout=$((SECONDS + 300))
+while :; do
+    state=$(get_job_state "$job_id")
+    if [[ -z $state ]]; then
+        print_full_test "$id" "$name" "FAILED" "$description" "$level" "Job ID $job_id either not found or has a different format."
+        break
+    fi
 
-if [[ $state == "W" ]]; then
-    print_full_test "$id" "$name" "FAILED" "$description" "$level" "$job_id still is W state. Please check the logs"
-elif [[ $state == "D" ]]; then
-    print_full_test "$id" "$name" "PASSED" "$description" "$level" "Job $job_id is in $state State."
-elif [[ $state == "ESV" ]]; then
-    print_full_test "$id" "$name" "FAILED" "$description" "Warning" "An error occured in saving the job output. This can be due to already existing output file/directory."
-else
-    print_full_test "$id" "$name" "FAILED" "$description" "$level" "Unexpected state : $state"
-fi
+    if [[ $state == "D" ]]; then
+        print_full_test "$id" "$name" "PASSED" "$description" "$level" "Job $job_id is in $state State."
+        break
+    elif [[ $state == "ESV" ]]; then
+        print_full_test "$id" "$name" "FAILED" "$description" "Warning" "An error occurred in saving the job output. This can be due to an already existing output file/directory."
+        break
+    elif [[ $state == "W" ]]; then
+        if [[ $SECONDS -gt $timeout ]]; then
+            print_full_test "$id" "$name" "FAILED" "$description" "$level" "Timeout reached. Job state did not transition to 'D' within 5 minutes."
+            break
+        else
+            echo "Current Job ID : $job_id"
+            "$JALIEN_PATH/jalien" -e ps
+            echo "Waiting for $(("$SECONDS" - "$start_time")) seconds"
+        fi
+    else
+        print_full_test "$id" "$name" "FAILED" "$description" "$level" "Unexpected state : $state"
+    fi
+done
